1aa5d6f90c318e3a85bfa5f58871774a06378982
rizky <rizky@avolut.com>
2025-04-20 20:02:33 +0700

    feat: implement API for publish.esensi with check endpoint; auto-generate related files

--- backend/src/gen/api.ts
+++ backend/src/gen/api.ts
@@ -1,7 +1,9 @@
 // Auto-generated API exports
 import type { ApiDefinitions } from "rlib/server";
-
+import { default as publish_esensi_check } from "../api/publish.esensi/check";
 
 export const backendApi = {
-
+  "publish.esensi": {
+    "check": ["/api/check", publish_esensi_check]
+  }
 } as const satisfies ApiDefinitions;
--- backend/src/gen/api.url.ts
+++ backend/src/gen/api.url.ts
@@ -2,5 +2,7 @@
 import type { ApiUrls } from "rlib/server";
 
 export const endpoints = {
-
+  "publish.esensi": {
+    "check": "/api/check"
+  }
 } as const satisfies ApiUrls;
