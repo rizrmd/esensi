f1b0ce2ff8d4cc67ea02c4474a4caf801224b481
Mieky Yudinata <miki211287@gmail.com>
2025-04-27 00:04:01 +0700

    refactor(publish): update layout structure and improve component organization

--- frontend/src/pages/publish.esensi/index.tsx
+++ frontend/src/pages/publish.esensi/index.tsx
@@ -2,36 +2,42 @@ import { Protected } from "@/components/app/protected";
 import { navigate } from "@/lib/router";
 import { Button } from "@/components/ui/button";
 import { baseUrl } from "@/lib/utils";
+import { AppLogo } from "@/components/app/logo";
 
 export default () => {
   const u = baseUrl();
   const content = (
-    <>
-      <div className="container">
-        <div className="flex flex-col items-center rounded-lg bg-accent p-8 text-center md:rounded-xl lg:p-16">
-          <h3 className="mb-3 max-w-3xl text-2xl font-semibold md:mb-4 md:text-4xl lg:mb-6">
-            Publish Esensi Online
-          </h3>
-          <p className="mb-8 max-w-3xl text-muted-foreground lg:text-lg">
-            Layanan Publikasi Buku Dari Penulis Secara Online
-          </p>
-          <div className="flex w-full flex-col justify-center gap-2 sm:flex-row">
-            <Button variant="outline" className="w-full sm:w-auto" asChild>
-              <a
-                href={`${u.auth}/register?callbackURL=${u.publish}/onboarding`}
-              >
-                Login
-              </a>
-            </Button>
-            <Button className="w-full sm:w-auto" asChild>
-              <a href={`${u.auth}/login?callbackURL=${u.publish}/dashboard`}>
-                Register
-              </a>
-            </Button>
+    <div className="grid min-h-svh">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <AppLogo />
+        </div>
+        <div className="">
+          <div className="flex flex-col items-center rounded-lg bg-accent p-8 text-center md:rounded-xl lg:p-16">
+            <h3 className="mb-3 max-w-3xl text-2xl font-semibold md:mb-4 md:text-4xl lg:mb-6">
+              Publish Esensi Online
+            </h3>
+            <p className="mb-8 max-w-3xl text-muted-foreground lg:text-lg">
+              Layanan Publikasi Buku Dari Penulis Secara Online
+            </p>
+            <div className="flex w-full flex-col justify-center gap-2 sm:flex-row">
+              <Button className="w-full sm:w-auto" asChild>
+                <a href={`${u.auth}/login?callbackURL=${u.publish}/dashboard`}>
+                  Login
+                </a>
+              </Button>
+              <Button variant="outline" className="w-full sm:w-auto" asChild>
+                <a
+                  href={`${u.auth}/register?callbackURL=${u.publish}/onboarding`}
+                >
+                  Register
+                </a>
+              </Button>
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
   return (
     <>
