15a0de66922e59e5134a9492186e949541ec9e39
Mieky Yudinata <miki211287@gmail.com>
2025-05-01 01:30:57 +0700

    Refactor: Standardize import statements and add semicolons across multiple UI components
    - Added semicolons to import statements in various files for consistency.
    - Updated function return statements to include semicolons.
    - Ensured consistent formatting in component props and function definitions.
    - Improved readability and maintainability of the codebase by adhering to a uniform style.

--- frontend/src/components/ui/spinner.tsx
+++ frontend/src/components/ui/spinner.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react'
+import * as React from "react";
 
-import { cn } from '@/lib/utils'
+import { cn } from "@/lib/utils";
 
 export interface LoadingSpinnerProps
   extends React.InputHTMLAttributes<HTMLDivElement> {
-  type?: 'short' | 'long' | 'bars'
+  type?: "short" | "long" | "bars";
 }
 
 const spinners = {
@@ -36,25 +36,25 @@ const spinners = {
         d="M136 32v32a8 8 0 0 1-16 0V32a8 8 0 0 1 16 0m37.25 58.75a8 8 0 0 0 5.66-2.35l22.63-22.62a8 8 0 0 0-11.32-11.32L167.6 77.09a8 8 0 0 0 5.65 13.66M224 120h-32a8 8 0 0 0 0 16h32a8 8 0 0 0 0-16m-45.09 47.6a8 8 0 0 0-11.31 11.31l22.62 22.63a8 8 0 0 0 11.32-11.32ZM128 184a8 8 0 0 0-8 8v32a8 8 0 0 0 16 0v-32a8 8 0 0 0-8-8m-50.91-16.4l-22.63 22.62a8 8 0 0 0 11.32 11.32l22.62-22.63a8 8 0 0 0-11.31-11.31M72 128a8 8 0 0 0-8-8H32a8 8 0 0 0 0 16h32a8 8 0 0 0 8-8m-6.22-73.54a8 8 0 0 0-11.32 11.32L77.09 88.4A8 8 0 0 0 88.4 77.09Z"
       />
     </svg>
-  )
-}
+  ),
+};
 const LoadingSpinner = React.forwardRef<HTMLDivElement, LoadingSpinnerProps>(
   ({ className, type, ...props }, ref) => {
     return (
       <div
         data-testid="spinner"
         className={cn(
-          'text-[color:var(--primary)] flex items-center justify-center animate-spin',
-          className ? className : 'h-6 w-6'
+          "text-[color:var(--primary)] flex items-center justify-center animate-spin",
+          className ? className : "h-6 w-6"
         )}
         ref={ref}
         {...props}
       >
-        {spinners[type ?? 'long']}
+        {spinners[type ?? "long"]}
       </div>
-    )
+    );
   }
-)
-LoadingSpinner.displayName = 'LoadingSpinner'
+);
+LoadingSpinner.displayName = "LoadingSpinner";
 
-export { LoadingSpinner as Spinner }
+export { LoadingSpinner as Spinner };
