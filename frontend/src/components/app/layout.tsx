d3c1b6772541159f9f6b2c760272f52557a64285
Eirudo <hi@eirudo.com>
2025-05-03 06:08:26 +0000

    Revert "Fix: Layout only for main.esensi in better way"
    This reverts commit 92e700cbc9c94a3b055a8e1c0e576764c9cf5125.

--- frontend/src/components/app/layout.tsx
+++ frontend/src/components/app/layout.tsx
@@ -1,5 +1,16 @@
 import type { FC, ReactNode } from "react";
+import { SidebarProvider, SidebarInset } from "../ui/sidebar";
+import { AppSidebar } from "./sidebar";
+import { AppHeader } from "./header";
 
 export const AppLayout: FC<{ children: ReactNode }> = ({ children }) => {
-  return <>{children}</>;
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <AppHeader />
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
+  );
 };
