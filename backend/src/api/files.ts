95c8140ca711cb05b714862a06c3391b30ad9359
Mieky Yudinata <miki211287@gmail.com>
2025-05-20 12:03:05 +0700

    refactor: streamline file upload handler by consolidating image checks and improving readability

--- backend/src/api/files.ts
+++ backend/src/api/files.ts
@@ -1,41 +1,44 @@
 import { join } from "node:path";
 import { defineAPI } from "rlib/server";
+import sharp from "sharp";
 
 export default defineAPI({
   name: "files",
   url: "/_file/upload/*",
   async handler(): Promise<Response> {
     const req = this.req!;
-    let bunFile = Bun.file(
-      join(
-        process.cwd(),
-        "_file/upload",
-        ...req.url.split("/_file/upload/").slice(1)
-      )
+    const fileName = req.url.split("/_file/upload/").slice(1).join("/");
+    const fileExtension = fileName.split(".").pop();
+    const isImage =
+      fileExtension === "jpg" ||
+      fileExtension === "jpeg" ||
+      fileExtension === "png" ||
+      fileExtension === "gif" ||
+      fileExtension === "webp";
+    const path = join(
+      process.cwd(),
+      "_file/upload",
+      ...req.url.split("/_file/upload/").slice(1)
     );
+    let bunFile = Bun.file(path);
     if (!(await bunFile.exists())) {
-      // check if file is an image
-      const fileName = req.url.split("/_file/upload/").slice(1).join("/");
-      const fileExtension = fileName.split(".").pop();
-      if (
-        fileExtension === "jpg" ||
-        fileExtension === "jpeg" ||
-        fileExtension === "png" ||
-        fileExtension === "gif" ||
-        fileExtension === "webp"
-      ) {
-        const path =
+      if (isImage) {
+        return Response.redirect(
           "https://esensi.online/_img/upload/" +
-          req.url.split("/_file/upload/").slice(1).join("/") +
-          "?w=350";
-        return Response.redirect(path);
+            req.url.split("/_file/upload/").slice(1).join("/") +
+            "?w=350"
+        );
       } else {
-        const path =
+        return Response.redirect(
           "https://esensi.online/_file/upload/" +
-          req.url.split("/_file/upload/").slice(1).join("/");
-        return Response.redirect(path);
+            req.url.split("/_file/upload/").slice(1).join("/")
+        );
       }
+    } else {
+      if (isImage) {
+        const image = await sharp(path).resize(350).toBuffer();
+        return new Response(image);
+      } else return new Response(bunFile);
     }
-    return new Response(bunFile);
   },
 });
