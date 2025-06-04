18da7384720d84c4fe5399ec4be3ed409778a632
rizky <rizky05@gmail.com>
2025-06-04 14:02:14 +0700

    fix: refactor store state management and improve readability

--- frontend/src/pages/main.esensi/store.tsx
+++ frontend/src/pages/main.esensi/store.tsx
@@ -1,5 +1,11 @@
-import { StoreCategories, type StoreCategoryItem } from "@/components/esensi/store-categories";
-import { StoreBooksCard, type StoreBooksCardItem } from "@/components/esensi/store-books-card";
+import {
+  StoreCategories,
+  type StoreCategoryItem,
+} from "@/components/esensi/store-categories";
+import {
+  StoreBooksCard,
+  type StoreBooksCardItem,
+} from "@/components/esensi/store-books-card";
 import { FeaturedBooks } from "@/components/esensi/featured-books";
 import { BooksByCategory } from "@/components/esensi/books-by-category";
 import { useLocal } from "@/lib/hooks/use-local";
@@ -9,48 +15,58 @@ import { MainEsensiLayout } from "@/components/esensi/layout";
 export default () => {
   const local = useLocal(
     {
-      cats_loading: true,
-      cats_list: [] as StoreCategoryItem[],
-      cats_selected: "Mbak Nurul" as string | null,
-      allbooks_loading: true,
-      allbooks_list: [] as StoreBooksCardItem[],
+      cats: {
+        loading: true,
+        list: [] as StoreCategoryItem[],
+        selected: "",
+      },
+      allbooks: {
+        loading: true,
+        list: [] as StoreBooksCardItem[],
+      },
     },
     async () => {
-      const res = await api.store({allbooks_cat: local.cats_selected});
-      local.cats_list = res.data.categories;
-      local.cats_loading = false;
+      const res = await api.store({ allbooks_cat: local.cats.selected });
 
-      local.allbooks_list = res.data.allbooks;
-      local.allbooks_loading = false;
+      local.cats.list = res.data.categories;
+      local.cats.loading = false;
 
+      local.allbooks.list = res.data.allbooks;
+      local.allbooks.loading = false;
 
-      local.cats_selected = "Harusnya ganti dongs";
-      
       local.render();
     }
   );
 
-  const changeStoreCategory = ( cat: string | null) => {
-    local.cats_selected = cat;
+  const changeStoreCategory = async (cat: string | null) => {
+    local.cats.loading = true;
     local.render();
-  }
+    // const res = await api.category({})
+    local.cats.selected = cat || "";
+    local.cats.loading = false;
+    local.render();
+  };
 
-  
   return (
     <MainEsensiLayout title="Toko Buku">
       <div className="w-full flex flex-col justify-center gap-10">
-
-        <h1>TES LOCAL APAKAH UPDATE: {local.cats_selected}</h1>
-
-        <StoreCategories action={changeStoreCategory} loading={local.cats_loading} list={local.cats_list} selected={local.cats_selected} />
+        <StoreCategories
+          action={changeStoreCategory}
+          loading={local.cats.loading}
+          list={local.cats.list}
+          selected={local.cats.selected}
+        />
         <div className="w-full flex flex-col justify-center gap-10 md:gap-20 lg:px-16 lg:pb-8">
-          <StoreBooksCard loading={local.allbooks_loading} list={local.allbooks_list} />
+          <StoreBooksCard
+            loading={local.allbooks.loading}
+            list={local.allbooks.list}
+          />
           <BooksByCategory
             category="parenting"
             title="Parenting"
             subtitle="Buku tentang parenting"
           />
-          
+
           <FeaturedBooks />
           <BooksByCategory
             category="parenting"
